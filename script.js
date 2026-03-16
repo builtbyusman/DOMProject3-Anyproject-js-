947e12193e353ebdf5c94cf1a8c1605d830753d5
Muhammad Usman <usmandeveloper13@gmail.com>
2026-03-16 16:59:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const task_btn = document.querySelector('#task-btn');
+task_btn.addEventListener('click', addTask);
+
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskText = input.value.trim();
+    if (taskText === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    li.addEventListener("click", () => {
+        li.classList.toggle("completed");
+    });
+
+    const delBtn = document.createElement("span");
+    delBtn.textContent = "❌";
+    delBtn.className = "delete";
+    delBtn.onclick = () => li.remove();
+    li.appendChild(delBtn);
+
+    document.querySelector("#taskList").appendChild(li);
+    input.value = "";
+}
